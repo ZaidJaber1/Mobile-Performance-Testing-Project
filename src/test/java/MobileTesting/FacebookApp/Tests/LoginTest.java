483e88a30d2af1d2dc2445c48be5eaad0ba8c8f6
Zaid Jaber <115215499+ZaidJaber1@users.noreply.github.com>
2025-09-18 14:36:46 +0300

    Add files via upload
    Login & Post Tests

--- src/test/java/MobileTesting/FacebookApp/Tests/LoginTest.java
+++ src/test/java/MobileTesting/FacebookApp/Tests/LoginTest.java
@@ -1,5 +1,54 @@
 package MobileTesting.FacebookApp.Tests;
 
-public class LoginTest {
+import MobileTesting.FacebookApp.Pages.LoginPage;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class LoginTest extends BaseTest {
+
+    private LoginPage loginPage;
+
+    @BeforeClass
+    public void initPage() {
+        loginPage = new LoginPage(driver);
+    }
+
+    @Test(priority = 1)
+    public void TC_invalidPassword() {
+        loginPage.clickAlreadyHaveAccount();
+        loginPage.enterEmail("eng.zaidjaber@gmail.com"); // valid email
+        loginPage.enterPassword("wrongpassword");       // invalid password
+        loginPage.clickLogin();
+        loginPage.handleInvalidPassword();             // go back to login
+        loginPage.clearFields();
+    }
+
+    @Test(priority = 2)
+    public void TC_bothInvalid() {
+        loginPage.enterEmail("invaliduser@example.com"); 
+        loginPage.enterPassword("wrongpassword");      
+        loginPage.clickLogin();
+        loginPage.handleBothInvalid();                // click TRY AGAIN
+        loginPage.clearFields();
+    }
+
+    @Test(priority = 3)
+    public void TC_invalidEmail() {
+        loginPage.enterEmail("invaliduser@example.com"); 
+        loginPage.enterPassword("validpassword");      
+        loginPage.clickLogin();
+        loginPage.handleInvalidEmail();               // click OK
+        loginPage.clearFields();
+    }
+
+    @Test(priority = 4)
+    public void TC_validLogin() {
+        loginPage.enterEmail("eng.zaidjaber@gmail.com");
+        loginPage.enterPassword("Alshaikh@2000");
+        loginPage.clickLogin();
+        // assertion for home page
+        Assert.assertTrue(loginPage.isHomePageDisplayed(), "Home page is not displayed, login failed!");
+    }
 
 }
--- src/test/java/MobileTesting/FacebookApp/Tests/PostTest.java
+++ src/test/java/MobileTesting/FacebookApp/Tests/PostTest.java
@@ -1,5 +1,23 @@
 package MobileTesting.FacebookApp.Tests;
 
-public class PostTest {
+import MobileTesting.FacebookApp.Pages.PostPage;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
+public class PostTest extends BaseTest {
+
+    private PostPage postPage;
+
+    @BeforeClass
+    public void initPage() {
+        postPage = new PostPage(driver);
+    }
+
+    @Test(priority = 1)
+    public void TC_createPost() {
+        postPage.clickCreateButton();
+        postPage.selectPostOption();
+        postPage.enterPostText("Hello, this is a test post from Appium!");
+        postPage.clickPostButton();
+    }
 }
